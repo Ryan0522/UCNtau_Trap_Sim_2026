729e0bbb091f22b1f48e6a66b6cb1ee30002cda4
Ryan0522 <ciyouh2@illinois.edu>
2026-03-16 16:17:53 -0500

    Started working on runner with MPI

--- src/core/runner.cpp
+++ src/core/runner.cpp
@@ -1,42 +1,30 @@
 #include "ucntrap/runner.hpp"
 #include "ucntrap/config.hpp"
 
+#include <mpi.h>
 #include <iostream>
+#include <memory>
 
 namespace ucntrap {
 
 Runner::Runner(SimulationConfig config) : config_(config) {}
 
 int Runner::run() const {
-    std::cout << "UCNTrap simulation\n";
-    
-    std::cout << " ============= Config ============= \n";
-    std::cout << "source = " << config_.source_type << "\n";
-    std::cout << "field model = " << config_.field_model << "\n";
-    std::cout << "integrator = " << config_.integrator << "\n";
-    std::cout << "tracker = " << config_.tracker << "\n";
-    std::cout << "writer = " << config_.writer << "\n";
-    std::cout << "output_prefix = " << config_.output_prefix << "\n";
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    std::cout << " ============= Parameters ============= \n";
-    std::cout << "dt = " << config_.dt << "\n";
-    std::cout << "ntraj = " << config_.ntraj << "\n";
-    std::cout << "clean time = " << config_.cleaning_time << "\n";
-    std::cout << "clean height = " << config_.cleaning_height << "\n";
-    std::cout << "raised clean height = " << config_.raised_cleaning_height << "\n";
-    std::cout << "hold time = " << config_.hold_time << "\n";
-
-    std::cout << "dip heights = <";
-    for (const auto& i : config_.dip_heights) {
-        std::cout << i << ", ";
+    // 1. Calculate neutron counts in this rank
+    size_t total_ntraj = config_.ntraj;
+    size_t my_ntraj = total_ntraj / size;
+    if (rank == size - 1) {
+        my_ntraj += (total_ntraj % size);
     }
-    std::cout << ">\n";
 
-    std::cout << "dip end times = <";
-    for (const auto& i : config_.dip_end_times) {
-        std::cout << i << ", ";
+    if (rank == 0) {
+        std::cout << "Starting Simulation with " << size << " ranks." << std::endl;
+        std::cout << "Total Trajectories: " << total_ntraj << std::endl;
     }
-    std::cout << ">\n";
 
     return 0;
 }
